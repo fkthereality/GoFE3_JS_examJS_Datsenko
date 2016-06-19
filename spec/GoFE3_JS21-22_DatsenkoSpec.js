2990047189301ab85e724d079f18532739be44be
Vladimir Datsenko <Vladimir Datsenko>
2016-06-19 20:56:17 +0300

    GoFE3_JS_exam_Datsenko deleted all support files

--- spec/GoFE3_JS21-22_DatsenkoSpec.js
+++
@@ -1,7 +0,0 @@
-var load = require('../js/main.js');//запрос main.js => module.exports
-describe("", function () {
-        var elemP = load.pow(4,2);
-        it('', function(){
-            expect(elemP).toBe(16);
-        });
-});
